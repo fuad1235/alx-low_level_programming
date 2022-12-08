af106aeffb877f7fe6d1b98d9966ddb5c5a6939d
Fuad Lamin <realfuad123@gmail.com>
2022-12-08 15:21:10 -0800

    6-size.c commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main Print out the sizes of data types
+ * Return: 0
+*/
+int main(void)
+{
+	char c
+	int i;
+	long int l;
+	long long int v;
+	float f;
+
+	printf("size of char : %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int : %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of a long int : %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("size of a long long int : %lu byte(s)\n", (unsigned long)sizeof(v));
+	printf("size of a float : %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}

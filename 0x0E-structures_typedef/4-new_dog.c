29a7383dc5f7280d030fb1d5f2f9ba5d75aaa5d5
Fuad Lamin <realfuad123@gmail.com>
2023-01-16 22:05:06 -0800

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,80 +1,55 @@
 #include "dog.h"
-
-
-/**
-*_strlen - returns length of
-*a string
-*@str: string to be counted
-*Return: returns length of string
-*/
-int _strlen(char *str)
-{
-int len = 0;
-while (str)
-len++;
-
-return (len);
-}
-
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
-*_strcopy - copy string pointed by src
-*into dest variable
-*@dest:buffer storing string copy
-*@src: buffer storing string to copy
-*Return:returns copied string
-*/
-char *_strcopy(char *dest, char *src)
-{
-int index = 0;
+ * *new_dog - function creates a new dog of type dog_t
+ *
+ *@name: char *
+ *@age: float
+ *@owner: char *
+ *
+ * Return: *new_dog or NULL
+ */
 
-for (; src[index] ; index++)
-dest[index] = src[index];
-
-dest[index] = '\0';
-return (dest);
-}
-
-
-
-
-/**
-*new_dog - creates a new dog
-*@name: name of new dog
-*@age: age of new dog
-*@owner: owner of new dog
-*Return: returns NULL in case
-*of failure
-*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *doggo;
+	dog_t *pdog;
+	char *pname, *powner;
+	int lenname, lenowner, i, x;
 
-if (name == NULL || age < 0 || owner == NULL)
-return (NULL);
-
-doggo = malloc(sizeof(dog_t));
-if (doggo == NULL)
-return (NULL);
-
-doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name == NULL)
-{
-free(doggo);
-return (NULL);
-}
-
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (doggo->owner == NULL)
-{
-free(doggo->name);
-free(doggo);
-return (NULL);
-}
-
-doggo->name = _strcopy(doggo->name, name);
-doggo->age = age;
-doggo->owner = _strcopy(doggo->owner, owner);
-
-return (doggo);
+	for (lenname = 0; name[lenname] != '\0'; lenname++)
+		;
+	lenname++;
+	for (lenowner = 0; owner[lenowner] != '\0'; lenowner++)
+		;
+	lenowner++;
+	pname = malloc(sizeof(char) * lenname);
+	if (pname == NULL)
+		return (NULL);
+	powner = malloc(sizeof(char) * lenowner);
+	if (powner == NULL)
+	{
+		free(pname);
+		return (NULL);
+	}
+	pdog = malloc(sizeof(dog_t));
+	if (pdog == NULL)
+	{
+		free(pname);
+		free(powner);
+		return (NULL);
+	}
+	for (i = 0; i < lenname; i++)
+	{
+		pname[i] = name[i];
+	}
+	for (x = 0; x < lenowner; x++)
+	{
+		powner[x] = owner[x];
+	}
+	pdog->name = pname;
+	pdog->age = age;
+	pdog->owner = powner;
+	return (pdog);
 }

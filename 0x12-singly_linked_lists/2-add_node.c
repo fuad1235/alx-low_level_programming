a2631530d9d85f989dde8912096b71a61c1ae6ea
Fuad Lamin <realfuad123@gmail.com>
2023-01-26 08:41:55 -0800

    message

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,21 +1,45 @@
 #include "lists.h"
-#include <stdlib.h>
-/**
-  * add_nodeint - add new nodes to the list
-  * @head: current place in the list
-  * @n: int to add to the list
-  * Return: pointer to current position in list
-  */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
 
-	new = *head;
-	new = malloc(sizeof(listint_t));
+int _strlen_recursion(char *s);
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
+	{
 		return (NULL);
-	new->n = n;
+	}
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen_recursion(new->str);
 	new->next = *head;
 	*head = new;
-	return (*head);
+
+	return (new);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,26 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
-  * free_listint - define function
-  * @head: describe argument
-  */
-void free_listint(listint_t *head)
+ * free_list - frees a list_t list.
+ * @head: pointer to the list.
+ **/
+void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	free_listint(head->next);
-	free(head);
+	list_t *actual_node;
+	list_t *next_node;
+
+	if (head)
+	{
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(actual_node->str);
+			free(actual_node);
+			actual_node = next_node;
+			next_node = next_node->next;
+		}
+		free(actual_node->str);
+		free(actual_node);
+	}
 }
